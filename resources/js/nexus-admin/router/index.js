c53e5e886ff683e27d22524a048386fbf6f04f60
lv <lv@lvdeMacBook-Pro.local>
2026-05-29 13:34:02 +0800

    cleanup: 移除 router/index.js 中未使用的 pageComponents 映射表

--- resources/js/nexus-admin/router/index.js
+++ resources/js/nexus-admin/router/index.js
@@ -4,16 +4,6 @@
  */
 import { createRouter, createWebHistory } from 'vue-router'
 
-// 页面组件映射表
-const pageComponents = {
-  'dashboard': () => import('../pages/demo/Dashboard.vue'),
-  'content-article': () => import('../pages/demo/ContentArticle.vue'),
-  'content-category': () => import('../pages/demo/ContentCategory.vue'),
-  'system-user': () => import('../pages/system/User.vue'),
-  'system-role': () => import('../pages/system/Role.vue'),
-  'system-config': () => import('../pages/system/Config.vue')
-}
-
 const routes = [
   {
     path: '/:pathMatch(.*)*',
@@ -27,5 +17,4 @@ const router = createRouter({
   routes
 })
 
-export { pageComponents }
 export default router
